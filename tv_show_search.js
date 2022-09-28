46a31044e368183c4e78351d487530130f853863
Ash <55725519+OnePunchFam@users.noreply.github.com>
2022-09-27 22:53:25 -0400

    TV Show Search
    - using axios library
    - TVMaze API
    - display corresponding images for resulting shows

--- tv_show_search.js
+++ tv_show_search.js
@@ -1,1 +1,33 @@
 // TV SHOW SEARCH APP
+
+
+// API:
+// http://api.tvmaze.com/search/shows?q=(queue you want to search)
+
+
+
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const userSearch = form.elements.query.value
+    const config = { params: { q: userSearch } }
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    displayImages(res.data)
+    form.elements.query.value = '';
+})
+
+
+
+const displayImages = (shows) => {
+    // create images for corresponding shows, append to page
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            // .show is coming from the API itself
+            document.body.append(img);
+        }
+
+    }
+}
+
